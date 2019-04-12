c2e4fbc304c94acbed6a6b2a1e2f1af37bd0d749
github username <github email address>
2019-04-11 19:29:58 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,8 +8,12 @@ class Post < ActiveRecord::Base
   def categories_attributes=(categories_hashes)
     # {"0"=>{"name"=>"new post 1"}, "1"=>{"name"=>" new post 2"}}
     categories_hashes.values.each do |category_hash|
-      category = Category.find_or_create_by(name: category_hash[:name])
-      self.post_categories.build(:category => category)
+      if category_hash[:name].present?
+        category = Category.find_or_create_by(name: category_hash[:name])
+        if !self.categories.include?(category)
+          self.post_categories.build(:category => category)
+        end
+      end
     end
   end
 
