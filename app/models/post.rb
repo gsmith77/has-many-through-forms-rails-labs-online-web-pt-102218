9b222b745263db9a0c288bf540576cba305af103
github username <github email address>
2019-04-11 20:18:55 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :comments, reject_if: :all_blank
 
   def categories_attributes=(categories_hashes)
     # {"0"=>{"name"=>"new post 1"}, "1"=>{"name"=>" new post 2"}}
